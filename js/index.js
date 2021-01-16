24d89eb0c0e2074a2391dea201a74348d9b8ee54
never9920 <929654604@qq.com>
2021-01-16 22:53:46 +0800

    饼状图

--- js/index.js
+++ js/index.js
@@ -85,11 +85,6 @@
         { year: "2019", data: [200, 300, 300, 900, 1500, 1200, 600] },
         { year: "2020", data: [300, 400, 350, 800, 1800, 1400, 700] }
     ];
-
-    $(".bar h2 ").on("click", "a", function() {
-        option.series[0].data = dataAll[$(this).index()].data;
-        myChart.setOption(option);
-    });
 })();
 
 // 折线图定制
@@ -200,6 +195,64 @@
     myChart.setOption(option);
 
     // 重新把配置好的新数据给实例对象
+    myChart.setOption(option);
+    window.addEventListener("resize", function() {
+        myChart.resize();
+    });
+})();
+
+(function() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.querySelector(".pie .chart"));
+
+    option = {
+        tooltip: {
+            trigger: "item",
+            formatter: "{a} <br/>{b}: {c} ({d}%)",
+            position: function(p) {
+                //其中p为当前鼠标的位置
+                return [p[0] + 10, p[1] - 10];
+            }
+        },
+        legend: {
+            top: "90%",
+            itemWidth: 10,
+            itemHeight: 10,
+            data: ["0岁以下", "20-29岁", "30-39岁", "40-49岁", "50岁以上"],
+            textStyle: {
+                color: "rgba(255,255,255,.5)",
+                fontSize: "12"
+            }
+        },
+        series: [{
+            name: "年龄分布",
+            type: "pie",
+            center: ["50%", "42%"],
+            radius: ["40%", "60%"],
+            color: [
+                "#065aab",
+                "#066eab",
+                "#0682ab",
+                "#0696ab",
+                "#06a0ab",
+                "#06b4ab",
+                "#06c8ab",
+                "#06dcab",
+                "#06f0ab"
+            ],
+            label: { show: false },
+            labelLine: { show: false },
+            data: [
+                { value: 1, name: "0岁以下" },
+                { value: 4, name: "20-29岁" },
+                { value: 2, name: "30-39岁" },
+                { value: 2, name: "40-49岁" },
+                { value: 1, name: "50岁以上" }
+            ]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
     window.addEventListener("resize", function() {
         myChart.resize();
